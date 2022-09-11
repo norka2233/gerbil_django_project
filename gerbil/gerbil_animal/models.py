e5e9c5b5409bec59f978467691cc18baeaeaadbe
Eleonora <norka2233@gmail.com>
2022-09-11 17:13:37 +0300

    created gerbil class with gerbil url

--- gerbil/gerbil_animal/models.py
+++ gerbil/gerbil_animal/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Gerbil(models.Mode):
+
+    GERBIL_COLOR = (
+        ('WH', 'white'),
+        ('GD', 'gold'),
+        ('BLCK', 'black'),
+        ('GR', 'grey'),
+        ('BN', 'brown')
+    )
+
+    GERBIL_SEX = (
+        ('f', 'female'),
+        ('m', 'male')
+    )
+    name = models.CharField(max_length=30)
+    color = models.CharField(max_length=1, choices=GERBIL_COLOR)
+    age = models.IntegerField()
+    sex = models.CharField(max_length=1, choices=GERBIL_SEX)
