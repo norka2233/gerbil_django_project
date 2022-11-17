52ef32fbc660724e12609721f0e1a0747b757935
Eleonora <norka2233@gmail.com>
2022-11-17 18:29:24 +0200

    fixed settings.py

--- gerbil/gerbil/settings.py
+++ gerbil/gerbil/settings.py
@@ -38,8 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gerbil_animal',
-    'gerbil_house',
 ]
 
 MIDDLEWARE = [
