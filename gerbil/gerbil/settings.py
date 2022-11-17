1511797b601ef0a2b24c67d5d32c2444a12efbd2
Eleonora <norka2233@gmail.com>
2022-11-17 13:23:04 +0200

    added grb_aml, grb_house app and added to stgs

--- gerbil/gerbil/settings.py
+++ gerbil/gerbil/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gerbil_animal',
+    'gerbil_house',
 ]
 
 MIDDLEWARE = [
---
+++ gerbil/gerbil_house/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class House(models.Model):
+
+    capacity  = models.IntegerField()
+    material =
