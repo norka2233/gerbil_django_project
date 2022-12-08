c01ff929dc4fb942a6594458cea4edc67cbe4873
Eleonora <norka2233@gmail.com>
2022-12-08 13:14:10 +0200

    fixed issue with TemplateDoesNotExist

--- gerbil/gerbil/settings.py
+++ gerbil/gerbil/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'gerbil.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'gerbil' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- gerbil/gerbil_animal/views.py
+++ gerbil/gerbil_animal/views.py
@@ -1,10 +1,14 @@
+import os
+base = os.path.abspath('gerbil/templates')
+
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hey, here is your gerbil!")
+    template = loader.get_template('gerbil_animal/index.html')
+    return HttpResponse(template.render())
 
 
 def gerbil_presentation(request, gerbil_id):
