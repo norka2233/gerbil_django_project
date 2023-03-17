05f148f6cd51bd1bf1d7b1c6eb7e06461004bbfe
Eleonora <norka2233@gmail.com>
2023-03-17 14:35:46 +0200

    alert message

---
+++ gerbil/gerbil/static/gerbil_specific/base.js
@@ -0,0 +1,12 @@
+var sleep = "Sleep";
+var checkboxFalse = "Please click on the checkbox";
+
+function check() { 
+
+    if(document.getElementById('checkBox').checked) {
+        alert(checkboxTrue);
+}
+    else {
+        alert(checkboxFalse);
+}
+}
