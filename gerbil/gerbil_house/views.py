8406e95fc2c4946a55f70e914c26c091a6bbe900
Eleonora <norka2233@gmail.com>
2023-03-20 17:24:41 +0200

    gerbil_house page template

--- gerbil/gerbil_house/views.py
+++ gerbil/gerbil_house/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import House
 
 
 def index(request):
-    return HttpResponse("Here is gerbil house")
+    gerbil_house = House.objects.values()
+    a = [house for house in gerbil_house]
+    context = {
+        'gerbil_house': gerbil_house,
+    }
+    template = loader.get_template('gerbil_house/base.html')
+    return HttpResponse(template.render(context, request))
