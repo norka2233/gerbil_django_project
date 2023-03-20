3b7314b97abcc2216906acd0c616af4edb8c9146
Eleonora <norka2233@gmail.com>
2023-03-20 16:31:16 +0200

    gerbil_cage page template

--- gerbil/gerbil_cage/views.py
+++ gerbil/gerbil_cage/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Cage
 
 
 def index(request):
-    return HttpResponse("Here is gerbil cage")
+    gerbil_cage = Cage.objects.values()
+    a = [gerbil for gerbil in gerbil_cage]
+    context = {
+        'gerbil_cage': gerbil_cage,
+    }
+    template = loader.get_template('gerbil_cage/base.html')
+    return HttpResponse(template.render(context, request))
